94f97d6cf444a1a6b29e1f988a539c1efa883646
andrehommrich <ah@creavo.de>
2025-08-14 00:15:06 +0200

    add

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,12 +1,15 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
+import axios from 'axios';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
-import { initializeTheme } from './composables/useAppearance';
 
+import { initializeTheme } from './composables/useAppearance';
+axios.defaults.baseURL = '/';
+axios.defaults.withCredentials = true;
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
