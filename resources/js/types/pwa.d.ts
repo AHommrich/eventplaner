d086919f118587edcf4cf9b30f886f8a4b902990
André Hommrich <108946792+AHommrich@users.noreply.github.com>
2025-08-31 14:22:18 +0200

    wip: docker/laravel setup iteration

---
+++ resources/js/types/pwa.d.ts
@@ -0,0 +1,10 @@
+// resources/js/types/pwa.d.ts
+declare module 'virtual:pwa-register' {
+  export function registerSW(options?: {
+    immediate?: boolean
+    onNeedRefresh?: () => void
+    onOfflineReady?: () => void
+    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void
+    onRegisterError?: (error: any) => void
+  }): void
+}
